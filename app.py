09938c9c3eebf86ce7a4ff5ac4979f118dbc4524
Almestin <davottakoyadres@gmail.com>
2026-06-04 13:23:45 +0200

    For the / path, we return an extended greeting. For other paths, we return a 404.

--- app.py
+++ app.py
@@ -2,13 +2,25 @@
 # shebang
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse
 
 class ImageHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-Type", "text/plain")
-        self.end_headers()
-        self.wfile.write(b"Hello from Image Hosting!")
+        parsed_path = urlparse(self.path)
+        if parsed_path.path == "/":
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain; charset=utf-8")
+            self.end_headers()
+            message = (
+                "Welcome to the image hosting service!\n"
+                "Use POST /upload to upload an image.\n"
+                "Uploaded images are available at: http://localhost:8080/images/<filename>\n"
+            )
+            self.wfile.write(message.encode('utf-8'))
+        else:
+            self.send_response(404)
+            self.end_headers()
+            self.wfile.write(b"Not Found")
 
 def run_server():
     server_address = ('0.0.0.0', 8000)
